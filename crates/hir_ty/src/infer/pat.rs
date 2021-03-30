e854f9e2bb899a72d404c4fe0ab36aa533fa24f8
Alexandru Macovei <alexnmaco@gmail.com>
2021-03-31 00:55:18 +0300

    Use Box'es to reduce the size of hir_def::expr::Pat from 112 to 64 bytes on 64bit

--- crates/hir_ty/src/infer/pat.rs
+++ crates/hir_ty/src/infer/pat.rs
@@ -170,7 +170,7 @@ impl<'a> InferenceContext<'a> {
                 TyKind::Ref(mutability, subty).intern(&Interner)
             }
             Pat::TupleStruct { path: p, args: subpats, ellipsis } => self.infer_tuple_struct_pat(
-                p.as_ref(),
+                p.as_deref(),
                 subpats,
                 expected,
                 default_bm,
@@ -178,7 +178,7 @@ impl<'a> InferenceContext<'a> {
                 *ellipsis,
             ),
             Pat::Record { path: p, args: fields, ellipsis: _ } => {
-                self.infer_record_pat(p.as_ref(), fields, expected, default_bm, pat)
+                self.infer_record_pat(p.as_deref(), fields, expected, default_bm, pat)
             }
             Pat::Path(path) => {
                 // FIXME use correct resolver for the surrounding expression
